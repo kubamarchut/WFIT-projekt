2c17ca37880697738e9264fcd9bc04978ebc8b05
Jakub Marchut <kuba.marchut@gmail.com>
2022-06-17 11:32:09 +0200

    improving canvas resizing

---
+++ js/resize.js
@@ -0,0 +1,9 @@
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
+
+function resizeCanvas(){
+  c.width = c.offsetWidth;
+  c.height = c.offsetHeight;
+  canvasWidth = c.width;
+  canvasHeight = c.height;
+}
